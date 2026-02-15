55b3acc1df70d1e189c309459ffd539a992f09ac
aminehabchi <amine.habchi.01@gmail.com>
2026-02-15 12:18:10 +0000

    create navbar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { SellerDashboard } from './pages/seller-dashboard/seller-dashboard';
 import { NotFound } from './pages/not-found/not-found';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
+import { Profile } from './pages/profile/profile';
 
 export const routes: Routes = [
     { path: '', component: Home },
-    { path: 'seller-dashboard', component: SellerDashboard },
+    { path: 'dashboard', component: SellerDashboard },
     { path: 'login', component: Login },
     { path: 'register', component: Register },
+    { path: 'profile', component: Profile },
     { path: 'not-found', component: NotFound },
     { path: '**', redirectTo: 'not-found' }
 ];
--- frontend/src/app/sub-components/navbar/navbar.ts
+++ frontend/src/app/sub-components/navbar/navbar.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+ isOpen = false;
 
+  toggleMenu() {
+    this.isOpen = !this.isOpen;
+  }
 }
